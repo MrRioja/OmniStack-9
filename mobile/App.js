e8a649554e9751a32d2cfce5632ce39cba8e6449
Luiz Rioja <lulyrioja@gmail.com>
2020-02-02 20:55:29 -0300

    App mobile do AirCnC
    Parte mobile do aplicativo do AirCnC

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
+
+export default function App() {
+  return <Routes />
+}
