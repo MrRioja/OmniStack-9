8d6e7abda6d62a9c7cb511dce891cbc295b4f8e3
Luiz Rioja <lulyrioja@gmail.com>
2022-09-30 19:11:15 -0300

    fix: correctly thumbnail rendering

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { withNavigation } from 'react-navigation';
+import React, { useState, useEffect } from "react";
+import { withNavigation } from "react-navigation";
 
-import { View, StyleSheet, Text, FlatList, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-import api from '../services/api';
+import api from "../services/api";
 
 function SpotList({ tech, navigation }) {
   const [spots, setSpots] = useState([]);
 
   useEffect(() => {
     async function loadSpots() {
-      const response = await api.get('/spots', {
-        params: { tech }
-      })
+      const response = await api.get("/spots", {
+        params: { tech },
+      });
 
       setSpots(response.data);
     }
@@ -21,32 +28,42 @@ function SpotList({ tech, navigation }) {
   }, []);
 
   function handleNavigate(id) {
-    navigation.navigate('Book', { id });
+    navigation.navigate("Book", { id });
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+      <Text style={styles.title}>
+        Empresas que usam <Text style={styles.bold}>{tech}</Text>
+      </Text>
 
       <FlatList
         style={styles.list}
         data={spots}
-        keyExtractor={spot => spot._id}
+        keyExtractor={(spot) => spot._id}
         horizontal
         showsHorizontalScrollIndicator={false}
         renderItem={({ item }) => (
           <View style={styles.listItem}>
-            <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+            <Image
+              style={styles.thumbnail}
+              source={{ uri: `http://10.0.0.199:3333/files/${item.thumbnail}` }}
+            />
             <Text style={styles.company}>{item.company}</Text>
-            <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-            <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
+            <Text style={styles.price}>
+              {item.price ? `R$${item.price}/dia` : "GRATUITO"}
+            </Text>
+            <TouchableOpacity
+              onPress={() => handleNavigate(item._id)}
+              style={styles.button}
+            >
               <Text style={styles.buttonText}>Solicitar reserva</Text>
             </TouchableOpacity>
           </View>
         )}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,13 +73,13 @@ const styles = StyleSheet.create({
 
   tittle: {
     fontSize: 20,
-    color: '#444',
+    color: "#444",
     paddingHorizontal: 20,
     marginBottom: 15,
   },
 
   bold: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 
   list: {
@@ -76,38 +93,37 @@ const styles = StyleSheet.create({
   thumbnail: {
     width: 200,
     height: 120,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 2,
   },
 
   company: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: "bold",
+    color: "#333",
     marginTop: 10,
   },
 
   price: {
     fontSize: 15,
-    color: '#999',
+    color: "#999",
     marginTop: 5,
   },
 
   button: {
     height: 32,
-    backgroundColor: '#f05a5b',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#f05a5b",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 2,
     marginTop: 15,
   },
 
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 15,
   },
-
 });
 
 export default withNavigation(SpotList);
